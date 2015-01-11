6785058015129a24f22b54e1ce8ab0daa361a75f
morontt <morontt@gmail.com>
2015-01-11 14:17:32 +0000

    change assets

---
+++ asset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+currentdate=`date '+%Y%m%d'`
+
+#touch ./www/css/temp.css
+
+cat ./www/css/all.css > ./www/css/temp.css
+echo "" >> ./www/css/temp.css
+cat ./www/css/main.css >> ./www/css/temp.css
+
+java -jar ./bin/yuicompressor.jar ./www/css/temp.css -o ./www/css/main_$currentdate.min.css --charset utf-8
+
+rm ./www/css/temp.css
